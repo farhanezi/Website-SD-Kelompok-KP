8ac53e76c21c442ed4e8a4fdce82385716dad70c
farhanezi <esafitrioktorini@gmail.com>
2026-06-19 10:50:50 +0700

    benerin merge

--- web-sekolah/routes/web.php
+++ web-sekolah/routes/web.php
@@ -4,13 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminDashboardController;
 
 Route::get('/', function () {
-<<<<<<< HEAD
-=======
     if (auth()->check()) {
         return redirect()->route('admin.dashboard');
     }
 
->>>>>>> b9c65e3216be6bf237075bb8b550291e668d0ec8
     return view('home');
 });
 
@@ -23,5 +20,3 @@ Route::prefix('admin')->name('admin.')->group(function () {
         Route::get('dashboard', [AdminDashboardController::class, 'index'])->name('dashboard');
     });
 });
-//ubah kode
-//saya melakukan perubahan kode disini
